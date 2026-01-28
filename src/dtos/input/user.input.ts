b3754c0b9fcb8c51559062c1a9a271a6a0054e9d
Daniel Levi <daniel@sistemasconfitec.com.br>
2026-01-28 08:27:41 -0300

    feat: adiciona resolver de criacao de usuario

---
+++ src/dtos/input/user.input.ts
@@ -0,0 +1,19 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class CreateUserInput {
+  @Field(() => String)
+  name!: string;
+
+  @Field(() => String)
+  email!: string;
+}
+
+// @InputType()
+// export class LoginInput {
+//   @Field(() => String)
+//   email!: string;
+
+//   @Field(() => String)
+//   password!: string;
+// }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,23 @@
 import { prismaClient } from "../../prisma/prisma";
+import type { CreateUserInput } from "../dtos/input/user.input";
 export class UserService {
+  async createUser(data: CreateUserInput) {
+    const findUser = await prismaClient.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (findUser) throw new Error("usuario ja cadastrado");
+
+    return await prismaClient.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+      },
+    });
+  }
+
   async finUser(id: string) {
     const user = await prismaClient.user.findUnique({
       where: {
